e8c3575959af333cc5e55f2a7600d8950f8e4184
aidyp <aidan.j.patrick@gmail.com>
2018-11-04 16:51:23 +0000

    code from tut

--- cordapp-template-kotlin/cordapp/src/test/kotlin/com/template/ContractTests.kt
+++ cordapp-template-kotlin/cordapp/src/test/kotlin/com/template/ContractTests.kt
@@ -1,6 +1,7 @@
 package com.template
 
 import net.corda.testing.node.MockServices
+import net.corda.testing.node.makeTestIdentityService
 import org.junit.Test
 
 class ContractTests {
@@ -8,6 +9,27 @@ class ContractTests {
 
     @Test
     fun `dummy test`() {
+
+    }
+}
+class FinanceContractTests {
+    private val ledgerServices = MockServices(
+            // A list of packages to scan for cordapps
+            cordappPackages = listOf("net.corda.finance.contracts"),
+            // The identity represented by this set of mock services. Defaults to a test identity.
+            // You can also use the alternative parameter initialIdentityName which accepts a
+            // [CordaX500Name]
+            initialIdentity = megaCorp,
+            // An implementation of IdentityService, which contains a list of all identities known
+            // to the node. Use [makeTestIdentityService] which returns an implementation of
+            // [InMemoryIdentityService] with the given identities
+            identityService = makeTestIdentityService(megaCorp.identity)
+    )
+
+    fun emptyLedger() {
+        ledger {
+            
+        }
 
     }
 }
